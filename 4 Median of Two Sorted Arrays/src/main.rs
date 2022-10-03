da390bce30647d7226add925f7f1145f7bf9769b
Jerebtw <jeremialp41@gmail.com>
2022-10-04 01:46:31 +0200

    Make 4 smaller / faster

--- 4 Median of Two Sorted Arrays/src/main.rs
+++ 4 Median of Two Sorted Arrays/src/main.rs
@@ -15,16 +15,15 @@ impl Solution {
   pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     let mut nums1 = nums1;
     let mut nums2 = nums2;
-    let mut both = vec![];
-    both.append(&mut nums1);
-    both.append(&mut nums2);
-    both.sort();
+    nums1.append(&mut nums2);
+    nums1.sort();
 
-    let len_both = both.len();
+    let len_both = nums1.len();
+    let half = len_both / 2;
     if len_both % 2 == 0 {
-      return ((both[len_both / 2 - 1] as f64) + (both[len_both / 2] as f64)) / 2.0;
+      return ((nums1[half - 1] as f64) + (nums1[half] as f64)) / 2.0;
     } else {
-      return both[len_both / 2] as f64;
+      return nums1[half] as f64;
     }
   }
 }
