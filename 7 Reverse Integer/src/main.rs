29b1c317a9c6718f6132d76b58ae296e2f2ebf29
Jerebtw <jeremialp41@gmail.com>
2022-10-04 02:53:09 +0200

    Add 7

---
+++ 7 Reverse Integer/src/main.rs
@@ -0,0 +1,33 @@
+#[allow(arithmetic_overflow)]
+fn main() {
+  println!("{}", Solution::reverse(123) == 321);
+  println!("{}", Solution::reverse(321) == 123);
+  println!("{}", Solution::reverse(-321) == -123);
+}
+
+struct Solution {}
+
+impl Solution {
+  pub fn reverse(x: i32) -> i32 {
+    let is_negative = x.is_negative();
+    let mut val = x;
+
+    if is_negative {
+      val = -x;
+    }
+
+    let mut res = val
+      .to_string()
+      .chars()
+      .rev()
+      .collect::<String>()
+      .parse::<i32>()
+      .unwrap_or(0);
+
+    if is_negative {
+      res = -res;
+    }
+
+    res
+  }
+}
